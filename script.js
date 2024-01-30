9e27dcd9bee5ef6e39459a4413f31ceb3ceeba9a
Matthew Garner <1matthewgarner@gmail.com>
2024-01-30 21:16:03 +0000

    Re-size grid based on user input

--- script.js
+++ script.js
@@ -10,22 +10,37 @@ function createSquares(numRows) {
 
         container.appendChild(newSquare);
     }
+
+    // Change colour when mouse enters square
+    const gridSquares = document.querySelectorAll('.grid-square');
+
+    gridSquares.forEach((square) => {
+        square.addEventListener('mouseenter', setSquareVisited);
+    }
+    );
 }
 
 function setSquareVisited (event) {
     event.target.classList.add('visited');
-
 }
 
 //Create the sketching area
 createSquares(16);
 
 
-// Change colour when mouse enters square
-const gridSquares = document.querySelectorAll('.grid-square');
 
-gridSquares.forEach((square) => {
-    square.addEventListener('mouseenter', setSquareVisited);
-}
-);
 
+//Call createSquares when button clicked
+const gridSizeButton = document.querySelector('.set-grid-size');
+
+gridSizeButton.addEventListener('click', () => {
+    const gridSize = prompt('How many Rows?');
+
+    if (gridSize < 100 && gridSize > 1) {
+        const container = document.querySelector('.main-container');
+        container.textContent = '';
+        createSquares(gridSize);
+    } 
+
+    
+})
