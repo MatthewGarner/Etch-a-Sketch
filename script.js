22b7b8cf43c5689e6bc47ea230c790d4ed5e7796
Matthew Garner <1matthewgarner@gmail.com>
2024-01-30 20:48:00 +0000

    Change colour when mouse enters grid squares

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-function createSquares(numSquares) {
+//Generate an n * n set of divs given input of n
+
+function createSquares(numRows) {
     const container = document.querySelector('.main-container');
+    const numSquares = numRows ** 2;
 
     for (let i = 0; i < numSquares; i++) {
         const newSquare = document.createElement('div');
@@ -9,4 +12,20 @@ function createSquares(numSquares) {
     }
 }
 
-createSquares(16 * 16);
+function setSquareVisited (event) {
+    event.target.classList.add('visited');
+
+}
+
+//Create the sketching area
+createSquares(16);
+
+
+// Change colour when mouse enters square
+const gridSquares = document.querySelectorAll('.grid-square');
+
+gridSquares.forEach((square) => {
+    square.addEventListener('mouseenter', setSquareVisited);
+}
+);
+
